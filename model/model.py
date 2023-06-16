24ec08a9233294616e0cf89035ccd32b05702ec2
minorchange <bnu@posteo.org>
2023-06-16 12:42:24 +0200

    Make the whole containerization morking
    for the first time using the example model of the parent repo and not
    the dummy repo of the submodule.

--- model/model.py
+++ model/model.py
@@ -1,4 +1,6 @@
-from create_and_train_clf import create_and_train_iris_clf
+import ast
+from model.create_and_train_clf import create_and_train_iris_clf
+import numpy as np
 
 
 class model_example:
@@ -12,5 +14,14 @@ class model_example:
     def fit(self, X, y):
         assert False, "This is a pretrained model"
 
-    def predict(self, X):
-        return self.clf.predict(X)
+    def predict(self, s):
+        print("Look here")
+        print(s)
+        X = ast.literal_eval(s)
+        X = np.array(X)
+        print(X)
+        if len(X) != 4:
+            return "Unexpected shape."
+        if len(np.shape(X)) == 1:
+            X = np.array([X])
+        return self.clf.predict(X).tolist()
